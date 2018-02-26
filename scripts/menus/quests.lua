4afe1681414c501710bd44aaf7f32d72c18bb39a
BeWorld2018 <36823759+BeWorld2018@users.noreply.github.com>
2018-02-26 13:39:49 +0100

    more strings tranlatable

--- scripts/menus/quests.lua
+++ scripts/menus/quests.lua
@@ -203,10 +203,10 @@ function RunQuestMenu(world)
 
 				confirm:resize(288,128)
 
-				confirm:addLabel("Delete " .. GetCurrentCustomHero(), 288 / 2, 11)
-				confirm:addLabel("Are you sure? This cannot be undone.", 288 / 2, 45, Fonts["game"])
+				confirm:addLabel(_("Delete") .. " " .. GetCurrentCustomHero(), 288 / 2, 11)
+				confirm:addLabel(_("Are you sure?") .. " This cannot be undone.", 288 / 2, 45, Fonts["game"])
 
-				confirm:addHalfButton("~!Yes", "y", 1 * (288 / 3) - 90, 120 - 16 - 27,
+				confirm:addHalfButton(_("~!Yes"), "y", 1 * (288 / 3) - 90, 120 - 16 - 27,
 					function()
 						DeleteCustomHero(GetCurrentCustomHero())
 						confirm:stop()
@@ -214,7 +214,7 @@ function RunQuestMenu(world)
 					end
 				)
 
-				confirm:addHalfButton("~!No", "n", 3 * (288 / 3) - 116, 120 - 16 - 27,
+				confirm:addHalfButton(_("~!No"), "n", 3 * (288 / 3) - 116, 120 - 16 - 27,
 					function() confirm:stop() end
 				)
 
@@ -266,7 +266,7 @@ function addQuestIcon(quest, menu, x, y)
 			l:setCaption(quest_description)
 			
 			if (GetQuestData(quest, "Map") ~= "") then
-				quest_menu:addFullButton("~!Play Quest", "p", 176 - (224 / 2), 352 - 40 * 2,
+				quest_menu:addFullButton(_("~!Play Quest"), "p", 176 - (224 / 2), 352 - 40 * 2,
 					function()
 						RunningScenario = true
 						SetCurrentQuest(quest)
@@ -292,7 +292,7 @@ function addQuestIcon(quest, menu, x, y)
 					end
 				)
 			end
-			quest_menu:addFullButton("~!Close", "c", 176 - (224 / 2), 352 - 40 * 1,
+			quest_menu:addFullButton(_("~!Close"), "c", 176 - (224 / 2), 352 - 40 * 1,
 				function()
 					quest_menu:stop()
 				end
@@ -361,7 +361,7 @@ function Briefing(quest)
 	menu:add(sw, Video.Width / 2 - (l:getWidth() / 2), 80 * Video.Height / 480)
 
 	if (table.getn(GetQuestData(quest, "Objectives")) > 0) then
-		menu:addLabel("Objectives:", 372 * Video.Width / 640, 306 * Video.Height / 480, Fonts["large"], false)
+		menu:addLabel(_("Objectives:"), 372 * Video.Width / 640, 306 * Video.Height / 480, Fonts["large"], false)
 
 		local objectives = ""
 		table.foreachi(GetQuestData(quest, "Objectives"), function(k,v) objectives = objectives .. v .. "\n" end)
